757a2ec94dbca54a001c06e5587cc609b4b0869f
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-15 16:10:41 +0100

    Solution for the example 7

---
+++ ex07/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+    return sum;
+}
+addThree();
+
+function addFive() {
+    sum = sum + 5;
+    return sum;
+}
+addFive();
+
+// Only change code above this line
+module.exports = {
+    addThree,
+    addFive
+};
