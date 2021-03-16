5c7a80cfb0455530c712583ea08f419e2403868c
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-16 01:29:03 +0100

    Adding the missing ;

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -3,14 +3,12 @@ var dress = "little dress";
 
 function myDress() {
     var dress = "ball gown";
-
 // Only change code above this line
     return dress;
 }
 // Only change code below this line
 myDress();
-
 // Only change code above this line
-console.log(dress)
+console.log(dress);
 
 module.exports = myDress;
